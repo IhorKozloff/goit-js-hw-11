d7523cec8b2b5f2542339eda127d69b1656d944e
IhorKozloff <ik.eyeshield21@gmail.com>
2022-01-14 14:53:15 +0200

    еще чуть чуть

--- src/index.js
+++ src/index.js
@@ -1,40 +1,103 @@
 import './css/styles.css';
-import imgSample from './sample'
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import imgSample from './templates/sample'
 import imageFinder from './myAPIService'
 
 
+const lightbox = new SimpleLightbox('.gallery a', { close: "true" });
 
 const refs = {
     formEl: document.querySelector('.search-form'),
     btnEl: document.querySelector('button'),
     galleryEl: document.querySelector('.gallery'),
     loadMoreBtnEl: document.querySelector('.load-more'),
+    gallerySectionEl: document.querySelector('.gallery-section'),
+    scrollEl: document.querySelector('.back-home-btn'),
 }
 console.log(refs.galleryEl)
 
 // largeImageURL - ссылка на большое изображение.
-
+const timeOut = 0;
 const imageAPI = new imageFinder;
 
 
 
 
- function frontEndMAker (murkUp) {
-    refs.galleryEl.insertAdjacentHTML('beforeend', imgSample(murkUp));
-    console.log(murkUp)
+ function frontEndMAker (dataToMurkUp) {
+     
+    refs.galleryEl.insertAdjacentHTML('beforeend', imgSample(dataToMurkUp));
+
  };
 
 
 
 function onSubmitForm (event) {
     event.preventDefault();
+    refs.gallerySectionEl.classList.remove('error');
     refs.galleryEl.innerHTML = '';
     imageAPI.userRequestValue = event.target.elements.searchQuery.value;
-    imageAPI.searchRequest().then(frontEndMAker);
-    refs.loadMoreBtnEl.classList.add('active');
+
+    imageAPI.searchRequest().then(searchResult => {
+
+       const {itemArr, totalItem} = searchResult;
+  
+        if (itemArr.length === 0) {
+            throw new Error();
+        }
+        
+        Notiflix.Notify.success(`Hooray! We found ${totalItem} images.`);
+
+        frontEndMAker(itemArr);
+
+        refs.loadMoreBtnEl.classList.add('active');
+
+    }).catch(onSearchError);
+    
 };
 function onLoadMoreBtn () {
-    imageAPI.loadMore().then(frontEndMAker);
+    imageAPI.loadMore().then(onMoresearchResult => {
+
+  
+        if (onMoresearchResult.length === 0) {
+            throw new Error();
+        };
+
+        frontEndMAker(onMoresearchResult);
+    }).catch(inTheEeeeeeend);
 };
+
+
+
+function onSearchError () {
+    refs.galleryEl.innerHTML = '';
+    refs.loadMoreBtnEl.classList.remove('active');
+    refs.gallerySectionEl.classList.add('error');
+    
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+function inTheEeeeeeend () {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    refs.loadMoreBtnEl.classList.remove('active');
+};
+
 refs.formEl.addEventListener('submit', onSubmitForm);
 refs.loadMoreBtnEl.addEventListener('click', onLoadMoreBtn)
+
+
+window.onscroll = function () {
+    
+    if (document.body.scrollTop > document.documentElement.clientHeight) {
+        refs.scrollEl.style.opacity = "0";
+    } else {
+        refs.scrollEl.style.opacity = "1";
+    }
+    
+};
+function goUp () {
+    const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (top > 0) {
+        window.scrollBy(0, -100);
+    }
+};
--- src/myAPIService.js
+++ src/myAPIService.js
@@ -1,31 +1,47 @@
+const PIXABAY_API_KEY = 'key=25226390-6524195878b00f535edfb636d';
+const URL = 'https://pixabay.com/api/';
+
 export default class ImageAPIService {
     constructor () {
-        this.URL = 'https://pixabay.com/api/';
+        
         this.imageType = `image_type=photo`;
         this.orientation = `orientation=horizontal`;
         this.safesearch = `safesearch=true`;
         this.indexOfPage = 1;
         this.userSearchRequest = 1;
-        this.PIXABAY_API_KEY = 'key=25226390-6524195878b00f535edfb636d';
+        
     }
         
     searchRequest () {
+
         this.indexOfPage = 1;  
-    return fetch(`${this.URL}?${this.PIXABAY_API_KEY}&q=${this.userSearchRequest}&${this.imageType}&${this.orientation}&${this.safesearch}&per_page=40&page=${this.indexOfPage}`).then(response => {
-        return response.json();
-    }).then(result => {
-        return result.hits;
-    });
+
+        return fetch(`${URL}?${PIXABAY_API_KEY}&q=${this.userSearchRequest}&${this.imageType}&${this.orientation}&${this.safesearch}&per_page=40&page=${this.indexOfPage}`).then(response => {
+            return response.json();
+        }).then(result => {
+            
+            const resultData = {
+                itemArr: result.hits, 
+                totalItem: result.totalHits,
+            };
+            
+            return resultData;
+        });
 
 
     };
+
     loadMore () {
+
         this.indexOfPage += 1;  
-        return fetch(`${this.URL}?${this.PIXABAY_API_KEY}&q=${this.userSearchRequest}&${this.imageType}&${this.orientation}&${this.safesearch}&per_page=40&page=${this.indexOfPage}`).then(response => {
+
+        return fetch(`${URL}?${PIXABAY_API_KEY}&q=${this.userSearchRequest}&${this.imageType}&${this.orientation}&${this.safesearch}&per_page=40&page=${this.indexOfPage}`).then(response => {
             return response.json();
         }).then(result => {
+            
             return result.hits;
         });
+        
     }
     
     get userRequestValue() {
